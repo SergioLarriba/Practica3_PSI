b3bc9a8adc3c710c15c2c1259ca627d4ebacd593
jav1er8 <javiervv2003@gmail.com>
2024-03-17 17:28:02 +0100

    Añadido models.py

--- models/models.py
+++ models/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class myClassView(APIView):
+
+
+    def get(self, request):
+        #Devuelve json ya que hereda APIView
+        return Response({'message': 'Got some data!'})
