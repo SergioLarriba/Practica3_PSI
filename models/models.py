aa0e09d0cbb89e04fe0eeace199ee44b68fffff4
jav1er8 <javiervv2003@gmail.com>
2024-03-20 09:36:28 +0100

    Pasa los tests

--- models/models.py
+++ models/models.py
@@ -8,7 +8,6 @@ import chess
 
 class myClassView(APIView):
 
-
     def get(self, request):
         #Devuelve json ya que hereda APIView
         return Response({'message': 'Got some data!'})
@@ -17,7 +16,10 @@ class myClassView(APIView):
 class Player(AbstractUser):
     #Default user class, just in case we want to add something extra in he future
     # add extra fields here
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=-1)
+
+    def __str__(self):
+        return f'{self.username} ({self.rating})'
 
 class ChessGame(models.Model):
     STATUS_CHOICES = [
@@ -25,20 +27,24 @@ class ChessGame(models.Model):
         ('A','active'),
         ('F','finished')
     ]
+
+    DEFAULT_BOARD_STATE = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
     id = models.AutoField(primary_key=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='A')
-    board_state = models.TextField()  #Formato FEN
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    board_state = models.TextField(default=DEFAULT_BOARD_STATE)  #Formato FEN
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True)
-    time_control = models.IntegerField()  #Tiempo por movimiento
-    white_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='white_player')
-    black_player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='black_player')
+    timeControl = models.CharField(max_length=15, default='30')  #Tiempo por movimiento  ¿¿CHAR???????????
+    whitePlayer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='whitePlayer',null=True)
+    blackPlayer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='blackPlayer',null=True)
     winner = models.ForeignKey(Player, on_delete=models.SET_NULL, related_name='winner', null=True)
 
     def __str__(self):
         board = chess.Board(self.board_state)  #Crea un tablero de ajedrez con el estado actual
-        return f'Game {self.id}: {self.white_player.username} (White) vs {self.black_player.username} (Black) - Status: {self.get_status_display()}\n{board}'
-
+        whitePlayer = f'{self.whitePlayer.username} ({self.whitePlayer.rating})' if self.whitePlayer else 'unknown'
+        blackPlayer = f'{self.blackPlayer.username} ({self.blackPlayer.rating})' if self.blackPlayer else 'unknown'
+        return f'GameID=({self.id}) {whitePlayer} vs {blackPlayer}'
 
 #CASCADE: si se borra un jugador se borran todas las partidas en las que participa
 #SET_NULL: si se borra un jugador se pone a null el campo winner
@@ -49,18 +55,21 @@ class ChessMove(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     move_from = models.CharField(max_length=2)
     move_to = models.CharField(max_length=2)
-    promotion = models.CharField(max_length=1, null=True)
+    promotion = models.CharField(max_length=1, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.game.status != 'A':
-            raise ValueError('LA partida no está activa, no se puede mover...')
         
+        if self.game.status != 'active':
+            raise Exception('La partida no está activa')
+
         board = chess.Board(self.game.board_state)
         # Movimiento + Comprobación de si promociona
-        move = chess.Move.from_uci(self.move_from + self.move_to + (self.promotion if self.promotion else ''))
-
+        move = chess.Move.from_uci(self.move_from + self.move_to + (self.promotion if self.promotion not in [None, ''] else ''))
         if move not in board.legal_moves:
             raise ValueError('Movimiento no válido')
+    
+        if self.promotion == '':
+            self.promotion = None
         
         board.push(move)
         self.game.board_state = board.fen()
@@ -70,4 +79,6 @@ class ChessMove(models.Model):
         
 
     def __str__(self):
-        return f'Player {self.player} Movement: {self.move_from} to {self.move_to} Promotion: {self.promotion}'
+        if self.promotion:
+            return f'{self.player} {self.move_from} -> {self.move_to} {self.promotion}'
+        return f'{self.player}: {self.move_from} -> {self.move_to}'
