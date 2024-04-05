abb2a4200960fc5641d66e1ef8c570357ec5889e
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2024-04-05 19:59:59 +0200

    Update consumer.py -> Pasa hasta el test 10 (incluido)

--- models/consumer.py
+++ models/consumer.py
@@ -1,113 +1,186 @@
-import websocket
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.consumer import AsyncConsumer
+import chess 
+
+from models.models import *
 from channels.generic.websocket import WebsocketConsumer
-import asyncio
-import chess
+from rest_framework.authtoken.models import Token
+# async_to_sync 
+from asgiref.sync import async_to_sync
+import pdb 
 
-
-class Consumer(AsyncWebsocketConsumer):
-
-    #Ejecuta cuando establece conexion WebSocket, verifica token y registra user, si token invalido cierra conexion
-    async def connect(self,token,user):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f"chat_{self.room_name}"
-
-        #Compruebo token
-        if not await self.verify_token(token):
-            await self.close(code=444)  #444 codigo de error personalizado
-            return
-
-        #Registro usuario
-        if not await self.register_user(user):
-            await self.close(code=445)  #445 codigo de error personalizado
-            return
-
+class ChessConsumer(WebsocketConsumer):
+    
+    # 1º Petición -> acceder a la pagina  
+    def connect(self):
+        self.gameID = self.scope['url_route']['kwargs']['gameID']
+        self.room_group_name = str(self.gameID)  
+        self.token_key = self.scope['query_string'].decode()    
+        
         # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name #Creado automaticamente por channels
-        )
+        async_to_sync(self.accept()) 
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
+        
+        # Comprueba si el token es válido 
+        if self.validate_token(self.token_key) is False:
+            async_to_sync(self.send(text_data = json.dumps({
+                'type': 'error', 
+                'message': 'Invalid token. Connection not authorized.'
+            }))) 
+            async_to_sync(self.close()) 
+            return
+        
+        # Comprueba si el game id es válido 
+        if not ChessGame.objects.filter(id=self.room_group_name).exists():
+            async_to_sync(self.send(text_data = json.dumps({
+                'type': 'error', 
+                'message': f'Invalid game with id {self.room_group_name}'
+            }))) 
+            async_to_sync(self.close()) 
+            return
+        
+        self.user_id = self.get_token_from_user(self.token_key)
+        # Comprueba si el usuario es válido del game_id 
+        if self.validate_user_in_game(self.gameID) is False:
+            async_to_sync(self.send(text_data = json.dumps({
+                'type': 'error', 
+                'message': f'Invalid game with id {self.gameID}'
+            }))) 
+            async_to_sync(self.close()) 
+            return
+        
 
-        #Se controla el acceso aqui
-        await self.accept()
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
+        
+        # Enviar un mensaje después de aceptar la conexión
+        async_to_sync(self.send(text_data=json.dumps({
+            'type': 'game',
+            'message': 'OK',
+            'status': 'connected',
+            'playerID': self.user_id 
+        }))) 
+        return 
+        
+        
+    def disconnect(self, close_code):
+        """ 
+        This method is called when the WebSocket connection is closed. 
+        It removes the channel from the room group.
+        """
+        
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
 
-    # Verifica token
-    async def verify_token(self, token):
-        await self.send(text_data=json.dumps({'token': token}))
-        response = await self.receive()
-        return json.loads(response['text'])['status'] == 'ok'
+    
+    # 2º Peticion y cualquiera -> la maneja receive 
+    def receive(self, text_data):
+        # pdb.set_trace() 
+        data = json.loads(text_data)
+        message_type = data.get('type')
+        
+        game = ChessGame.objects.get(id=self.gameID)
 
+        if message_type == 'move':
+            # Extraccion de los datos 
+            _from = data.get('from')
+            to = data.get('to')
+            playerID = data.get('playerID')
+            promotion = data.get('promotion')
 
-    # Registra usuario
-    async def register_user(self, user):
-        await self.send(text_data=json.dumps({'topic': self.topic, 'user': user}))
-        response = await self.receive()
-        return json.loads(response['text'])['status'] == 'ok' 
-
-
-
-    #Si recibe "move" valida el movimiento y lo almacena. Luego lo envia y verifica si finaliza la partida
-    def receive(self, message):
-        data = json.loads(message)
-        message_type = data['message']
-
-        #En caso de que el mensaje sea "move", lo valida y envia a todos los jugadores (channel_layer)
-        #Luego verifica si la partida ha finalizado usar python-chess
-        if message_type == "move":
-            move_data = data.get('move_data')
-            if move_data:
-                #Validar y almacenar movimiento
-                await self.process_move(move_data)
-            else:
-                await self.send_error("Invalid move message")
-        else:
-            await self.send_error("Invalid message type")
-
-
-    #Procesa el movimiento, lo valida y lo almacena
-    async def process_move(self, move_data):
-        try:
-            move = chess.Move.from_uci(move_data['move'])
-            if move in self.board.legal_moves:
-                self.board.push(move)
-                await self.channel_layer.group_send(
-                    self.room_group_name,
+            # Si el movimiento es válido, lo almacena en la base de datos
+            try:
+                player = Player.objects.get(id=self.user_id)
+                game = ChessGame.objects.get(id=self.gameID)
+                chess_move = ChessMove(move_from=_from, move_to=to, game=game, player=player, promotion=promotion)
+                chess_move.save()
+            except Exception as e:
+                # Si hay un error al guardar el movimiento, envía un mensaje de error
+                async_to_sync(self.channel_layer.group_send(({
+                   self.room_group_name,
                     {
-                        'type': 'send_move',
-                        'message': move_data
-                    }
-                )
-                if self.check_game_end():
-                    await self.channel_layer.group_send(
-                        self.room_group_name,
-                        {
-                            'type': 'game_end',
+                        'type': 'move_cb',
+                        'content': {
+                            'type': 'error',
+                            'from': _from,
+                            'to': to,
+                            'playerID': playerID,
+                            'promotion': promotion 
                         }
-                    )
-            else:
-                await self.send_error("Invalid move")
-        except Exception as e:
-            await self.send_error(str(e))
-                
+                    }
+                })))
+                return
 
-    #Verifica si la partida ha finalizado con python-chess
-    def check_game_end(self):
-        return self.board.is_game_over()
+            # Envía el movimiento a todos los jugadores en el mismo juego
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'move_cb',
+                    'content':
+                    {
+                        'type': 'move',
+                        'from': _from,
+                        'to': to,
+                        'playerID': playerID,
+                        'promotion': promotion 
+                    }
+                }
+            )
 
-
-    #Envia error
-    async def send_error(self, message):
-        await self.send(text_data=json.dumps({
-            'type': 'error',
-            'message': message
-        }))
-
-    #Desconcexion
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        return
+            
+    def game_cb(self, event):
+        """ This method is called when the server sends a message to the WebSocket 
+        connection. It sends the message to the client. 
+        """
+        
+        message = event['content']['message']
+        status = event['content']['status']
+        playerID = event['content']['playerID'] 
+        
+        # Send message to WebSocket
+        async_to_sync(self.send(text_data=json.dumps({
+            self.room_group_name,{
+                'type': 'game',
+                'message': message, 
+                'status': status,
+                'playerID': playerID
+            }
+        })))
+        
+    def move_cb(self, event):
+        # Extrae los detalles del movimiento del evento
+        _from = event['content']['from']
+        to = event['content']['to']
+        playerID = event['content']['playerID']
+        promotion = event['content']['promotion']
+    
+        # Envía el movimiento a todos los jugadores en el mismo juego
+        async_to_sync(self.send(text_data=json.dumps({
+            'type': 'move',
+            'from': _from,
+            'to': to,
+            'playerID': playerID,
+            'promotion': promotion
+        })))
+    
+    # Obtener user_id a partir del token
+    def get_token_from_user(self, token_key):
+        try: 
+            token = Token.objects.get(key=token_key)
+            return token.user_id
+        except Token.DoesNotExist:
+            return None
+    
+    # Comprobar si el token es válido 
+    def validate_token(self, token_key):
+        return Token.objects.filter(key=token_key).exists()
+    
+    # Comprobar si el usuario en el game es valido 
+    def validate_user_in_game(self, game_id): 
+        if ChessGame.objects.filter(id=game_id, whitePlayer=self.user_id).exists() or \
+        ChessGame.objects.filter(id=game_id, blackPlayer=self.user_id).exists():
+            return True
+        return False
+            
+        
+            
+        
     
