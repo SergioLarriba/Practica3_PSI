40ba5baea07e2ed4ee8f09d95b4edc69490d2693
jav1er8 <javiervv2003@gmail.com>
2024-04-06 14:23:51 +0200

    Pasa flake8

--- models/routing.py
+++ models/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from .consumers import ChessConsumer
 
 websocket_urlpatterns = [
-    #Las peticiones http://localhost:8000/ws/chat/room_name/ seran manejadas por el Consumer
+    # Las peticiones http://localhost:8000/ws/chat/room_name/
+    # seran manejadas por el Consumer
     re_path(r'ws:\\host/ws/play/<int:gameID>/token/', ChessConsumer.as_asgi()),
 ]
