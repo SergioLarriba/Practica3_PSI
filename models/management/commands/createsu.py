5ec51ace3cd60669357a749697bdaed7ac99af2b
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2024-04-08 12:34:00 +0200

    Update createsu.py

--- models/management/commands/createsu.py
+++ models/management/commands/createsu.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from models.models import Player 
 from django.core.management.base import BaseCommand
 
 
@@ -6,8 +6,8 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='alumnodb').exists():
-            User.objects.create_superuser(
+        if not Player.objects.filter(username='alumnodb').exists():
+            Player.objects.create_superuser(
                 username='alumnodb',
                 password='alumnodb'
             )
