87abc2ce72a839fce5f84b59bf4f3d719cc23e57
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2024-04-08 11:32:22 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+# python3 manage.py collectstatic --no-input
+python3 manage.py migrate
+
+python3 manage.py createsu 
