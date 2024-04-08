bb439c633c2c745ceb02868e330e544ae7eeabb1
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2024-04-08 12:53:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 pip install python-dotenv
 
-# python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 
 python3 manage.py createsu 
