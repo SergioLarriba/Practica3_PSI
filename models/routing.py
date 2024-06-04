cfca52539cd1970225b5a1bad13c5a1c1e4d9252
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2024-06-04 17:33:08 +0200

    Update routing.py -> Link funcional

--- models/routing.py
+++ models/routing.py
@@ -4,5 +4,5 @@ from .consumers import ChessConsumer
 websocket_urlpatterns = [
     # Las peticiones http://localhost:8000/ws/chat/room_name/
     # seran manejadas por el Consumer
-    e_path(r'ws/play/(?P<gameID>\d+)/?', ChessConsumer.as_asgi()),
+    re_path(r'ws/play/(?P<gameID>\d+)/?', ChessConsumer.as_asgi()),
 ]
